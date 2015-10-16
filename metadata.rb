06d284c1c90f3020a7e2290483c3527ad00c8662
Keith Gable <keith@gallodigital.com>
2015-10-15 20:40:51 -0700

    Add a metadata.rb.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             "rlec"
+maintainer       "Keith Gable"
+maintainer_email "keith@art19.com"
+license          "MIT"
+description      "Installs Redis Labs Enterprise Cluster (RLEC)"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
